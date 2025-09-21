9d04c70ec18c5d680522d30a6b3e0f7cdeb20a11
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2025-09-21 11:03:29 +0200

    reset

--- src/NavBar/NavBarContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NavBar from './NavBar';
-import { appName } from '../utils/appDefinitions';
-
-const NavBarContainer = props => {
-  return <NavBar appName={appName} />;
-};
-
-export default NavBarContainer;
